551b8248555ef39d39a8f7c0eeb3fbbdeff4b637
Yashwant_Ray <93814966+Yashwant361@users.noreply.github.com>
2026-06-08 14:27:13 +0000

    added code

---
+++ testDocker/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+// app.get('/',function handleHomePage(req,res){
+//     res.send('Hello World')
+// })
+
+function handleHomePage(req,res){
+    res.send('Hello World')
+}
+
+app.get('/',handleHomePage);
+
+
+app.listen(8085, () => {
+    console.log('Server running on http://localhost:8085');
+});
